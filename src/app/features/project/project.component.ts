2e943b93790bc1b3aac7fa366a81f042271c1ada
farahmahfouz <farahmahfouz11@gmail.com>
2026-07-05 00:40:55 +0300

    refactor: seperate pagination code to another component

--- src/app/features/project/project.component.ts
+++ src/app/features/project/project.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { PaginationComponent } from "../../shared/components/pagination/pagination.component";
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, PaginationComponent],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
