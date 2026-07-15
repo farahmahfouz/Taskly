af444960e711c5d503493edb170844c63a509bd4
farahmahfouz <farahmahfouz11@gmail.com>
2026-07-15 20:29:02 +0300

    feat: implement epic list page ui

---
+++ src/app/features/epics/components/epic-card/epic-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epic-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './epic-card.component.html',
+  styleUrl: './epic-card.component.css'
+})
+export class EpicCardComponent {
+
+}
--- src/app/features/epics/epics.component.ts
+++ src/app/features/epics/epics.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PaginationComponent } from '../../shared/components/pagination/pagination.component';
+import { EpicCardComponent } from "./components/epic-card/epic-card.component";
 
 @Component({
   selector: 'app-epics',
   standalone: true,
-  imports: [],
+  imports: [PaginationComponent, EpicCardComponent],
   templateUrl: './epics.component.html',
   styleUrl: './epics.component.css',
 })
