f58dcf1362cbb8f6b49e1e0beba442cfcf6cf405
farahmahfouz <farahmahfouz11@gmail.com>
2026-07-16 03:26:22 +0300

    feat: imolement epic project pagination

--- src/app/features/epics/epics.component.ts
+++ src/app/features/epics/epics.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { PaginationComponent } from '../../shared/components/pagination/pagination.component';
 import { EpicCardComponent } from './components/epic-card/epic-card.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { EpicsService } from './epics.service';
 import { Epic } from './epic.model';
-import { SearchIconComponent } from "../../shared/icons";
-import { ProjectErrorComponent } from "../project/components/project-error/project-error.component";
-import { EmptyEpicsComponent } from "./components/empty-epics/empty-epics.component";
-import { SkeltonComponent } from "../project/components/skelton/skelton.component";
-import { SkeltonEpicsComponent } from "./components/skelton-epics/skelton-epics.component";
+import { SearchIconComponent } from '../../shared/icons';
+import { ProjectErrorComponent } from '../project/components/project-error/project-error.component';
+import { EmptyEpicsComponent } from './components/empty-epics/empty-epics.component';
+import { SkeltonComponent } from '../project/components/skelton/skelton.component';
+import { SkeltonEpicsComponent } from './components/skelton-epics/skelton-epics.component';
+import { HttpResponse } from '@angular/common/http';
+import { InfinteScrollDirective } from "../../shared/directives/infinte-scroll.directive";
 
 @Component({
   selector: 'app-epics',
   standalone: true,
-  imports: [PaginationComponent, EpicCardComponent, SearchIconComponent, ProjectErrorComponent, EmptyEpicsComponent, SkeltonEpicsComponent],
+  imports: [
+    PaginationComponent,
+    EpicCardComponent,
+    SearchIconComponent,
+    ProjectErrorComponent,
+    EmptyEpicsComponent,
+    SkeltonEpicsComponent,
+    RouterLink,
+    InfinteScrollDirective
+],
   templateUrl: './epics.component.html',
   styleUrl: './epics.component.css',
 })
@@ -22,6 +33,13 @@ export class EpicsComponent implements OnInit {
   epics: Epic[] = [];
   isError = false;
   isLoading = false;
+
+  currentPage = 1;
+  limit = 2;
+
+  totalCount = 0;
+  totalPages = 0;
+
   constructor(
     private route: ActivatedRoute,
     private epicsService: EpicsService,
@@ -30,15 +48,56 @@ export class EpicsComponent implements OnInit {
   ngOnInit(): void {
     this.projectId = this.route.snapshot.paramMap.get('id')!;
     if (this.projectId) {
-      this.getProjectEpics(this.projectId);
+      this.getProjectEpics();
     }
   }
 
-  getProjectEpics(projectId: string) {
-    this.isLoading = true;
-    this.epicsService.getAllProjectEpics(projectId).subscribe({
-      next: res => {
-        this.epics = res;
+  get offset() {
+    return (this.currentPage - 1) * this.limit;
+  }
+
+  changePage(page: number) {
+    if (page < 1 || page > this.totalPages) return;
+
+    this.currentPage = page;
+    this.getProjectEpics();
+  }
+
+  nextPage() {
+    this.changePage(this.currentPage + 1);
+  }
+
+  previousPage() {
+    this.changePage(this.currentPage - 1);
+  }
+
+  loadMore() {
+    if (this.isLoading) return;
+    if (this.currentPage >= this.totalPages) return;
+
+    this.currentPage++;
+    this.getProjectEpics(true);
+  }
+
+  getProjectEpics(mobileScreenLoader = false) {
+    if (mobileScreenLoader) {
+      this.isLoading = false;
+    } else {
+      this.isLoading = true;
+    }
+
+    this.epicsService.getAllProjectEpics(this.projectId, this.limit, this.offset).subscribe({
+      next: (res: HttpResponse<Epic[]>) => {
+        const newEpics = res.body ?? [];
+
+        this.epics = mobileScreenLoader ? [...this.epics, ...newEpics] : newEpics;
+
+        const contentRange = res.headers.get('Content-Range');
+
+        this.totalCount = Number(contentRange?.split('/')[1] ?? 0);
+
+        this.totalPages = Math.ceil(this.totalCount / this.limit);
+
         this.isLoading = false;
         this.isError = false;
       },
--- src/app/features/epics/epics.service.ts
+++ src/app/features/epics/epics.service.ts
@@ -4,17 +4,24 @@ import { CreateEpicRequest, Epic } from './epic.model';
 import { API } from '../../core/utils/constants';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EpicsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  createNewEpic(data: CreateEpicRequest){
+  createNewEpic(data: CreateEpicRequest) {
     return this.http.post<CreateEpicRequest>(`${API.EPICS}`, data);
   }
 
-  getAllProjectEpics(projectId: string){
-    return this.http.get<Epic[]>(`${API.PROJECT_EPICS}?project_id=eq.${projectId}`)
+  getAllProjectEpics(projectId: string, limit: number, offset: number) {
+    return this.http.get<Epic[]>(
+      `${API.PROJECT_EPICS}?project_id=eq.${projectId}&limit=${limit}&offset=${offset}`,
+      {
+        observe: 'response', // To retrieve all response not just body
+        headers: {
+          Prefer: 'count=exact', // To retrieve count of all projects and set the count to headers "Content-Range"
+        },
+      },
+    );
   }
 }
