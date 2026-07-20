90d98b9ee3bc67e7abc1c999df77c958d1cb2ab7
farahmahfouz <farahmahfouz11@gmail.com>
2026-07-21 00:44:51 +0300

    chore: temporarily rename guards folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './features/auth/signup/signup.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
-import { authGuard } from './core/guards/auth.guard';
+import { authGuard } from './core/Guards/auth.guard';
 import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './features/auth/reset-password/reset-password.component';
-import { guestGuard } from './core/guards/guest.guard';
+import { guestGuard } from './core/Guards/guest.guard';
 
 export const routes: Routes = [
   { path: '', component: SignupComponent, canActivate: [guestGuard], title: 'Sign Up' },
