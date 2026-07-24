7149fa51310ebbbe5b8e1a53d811d0a05658180e
farahmahfouz <farahmahfouz11@gmail.com>
2026-07-24 04:58:12 +0300

    feat: show data in popup up

--- src/app/features/tasks/components/task-popup/task-popup.component.ts
+++ src/app/features/tasks/components/task-popup/task-popup.component.ts
@@ -1,19 +1,23 @@
-import { Component, effect, EventEmitter, input, Input, Output } from '@angular/core';
+import { Component, effect, EventEmitter, input, Input, Output, signal } from '@angular/core';
 import { ModalComponent } from '../../../../shared/components/modal/modal.component';
-import { TextareaComponent } from '../../../../shared/components/textarea/textarea.component';
 import { CopyLinkIconComponent } from '../../../../shared/icons';
 import { TasksService } from '../../tasks.service';
+import { Task } from '../../task.constants';
+import { DatePipe } from '@angular/common';
+import { InitialsPipe } from '../../../../shared/pipes/initials.pipe';
 
 @Component({
   selector: 'app-task-popup',
   standalone: true,
-  imports: [ModalComponent, TextareaComponent, CopyLinkIconComponent],
+  imports: [ModalComponent, CopyLinkIconComponent, DatePipe, InitialsPipe],
   templateUrl: './task-popup.component.html',
   styleUrl: './task-popup.component.css',
 })
 export class TaskPopupComponent {
   @Input() isOpen = true;
   @Output() close = new EventEmitter<void>();
+
+  task = signal<Task | null>(null);
 
   projectId = input.required<string>();
   taskId = input.required<string>();
@@ -26,6 +30,7 @@ export class TaskPopupComponent {
       this.taskService.getTask(projectId, taskId).subscribe({
         next: res => {
           console.log(res);
+           this.task.set(res[0]);
         },
       });
     });
