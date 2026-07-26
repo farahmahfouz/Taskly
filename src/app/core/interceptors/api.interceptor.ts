2520a8bcdcf28a8678f15e00656964c167e803e7
farahmahfouz <farahmahfouz11@gmail.com>
2026-07-26 04:09:23 +0300

    refactor: inject app configuration using InjectionToken

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -1,22 +1,24 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../utils/enviroment';
+import { ENVIRONMENT } from '../utils/enviroment';
 import { STORAGE_KEYS } from '../utils/constants';
+import { inject } from '@angular/core';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const env = inject(ENVIRONMENT);
   const token =
     localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN) ||
     sessionStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN);
 
   const headers: Record<string, string> = {
-    apikey: environment.supabaseAnonKey,
+    apikey: env.supabaseAnonKey,
   };
 
   if (!req.headers.has('Authorization')) {
-    headers['Authorization'] = `Bearer ${token ?? environment.supabaseAnonKey}`;
+    headers['Authorization'] = `Bearer ${token ?? env.supabaseAnonKey}`;
   }
 
   const apiReq = req.clone({
-    url: environment.apiUrl + req.url,
+    url: env.apiUrl + req.url,
     setHeaders: headers,
   });
 
--- src/app/core/utils/enviroment.ts
+++ src/app/core/utils/enviroment.ts
@@ -1,4 +1,16 @@
-export const environment = {
+import { InjectionToken } from '@angular/core';
+
+export interface Environment {
+  apiUrl: string;
+  supabaseAnonKey: string;
+}
+
+export const environment: Environment = {
   apiUrl: 'https://fxauwbhgcedymmgoscxv.supabase.co',
   supabaseAnonKey: 'sb_publishable_54tl36MeXBqke41iY28O0A_qLbnyo-C',
 };
+
+export const ENVIRONMENT = new InjectionToken<Environment>('app.environment', {
+  providedIn: 'root',
+  factory: () => environment,
+});
