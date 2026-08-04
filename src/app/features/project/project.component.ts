707e6f1fba6754dbf94e46df5b8eb3a83ec8ea60
farahmahfouz <farahmahfouz11@gmail.com>
2026-08-04 18:20:25 +0300

    refact: use svg icon in add project box

--- src/app/features/project/project.component.ts
+++ src/app/features/project/project.component.ts
@@ -13,6 +13,7 @@ import { ProjectContextService } from '../../core/services/project-context.servi
 import { ErrorPageComponent } from '../../shared/components/error-page/error-page.component';
 import { PaginationBase } from '../../shared/classes/pagination.base';
 import { LoaderComponent } from "../../shared/components/loader/loader.component";
+import { PlusCircleIconComponent } from "../../shared/icons/plus-circle-icon.component";
 
 @Component({
   selector: 'app-project',
@@ -25,7 +26,8 @@ import { LoaderComponent } from "../../shared/components/loader/loader.component
     EmptyProjectsComponent,
     InfinteScrollDirective,
     ErrorPageComponent,
-    LoaderComponent
+    LoaderComponent,
+    PlusCircleIconComponent
 ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css',
@@ -64,7 +66,6 @@ export class ProjectComponent extends PaginationBase implements OnInit {
           const newProjects = res.body ?? [];
 
           this.projects = loadMore ? [...this.projects, ...newProjects] : newProjects;
-          // this.projects = [];
           const contentRange = res.headers.get('Content-Range');
 
           this.totalCount = Number(contentRange?.split('/')[1] ?? 0);
